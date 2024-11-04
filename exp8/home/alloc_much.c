3cfe9c50ce6f922dffe702d1f68e265ac3ac1f12
yyhhenry <yyhhenry@foxmail.com>
2024-11-04 22:57:54 +0800

    feat: update alloc_much

--- exp8/home/alloc_much.c
+++ exp8/home/alloc_much.c
@@ -6,24 +6,24 @@ int main()
 {
 	int i, j;
 	int size;
-	char *p;
+	char *p0, *p1;
 	{
 		printf("alloc_much: allocating 8MB memory\n");
 		size = 8 * 1024 * 1024;
-		p = malloc(size);
+		p0 = malloc(size);
 		for (j = 0; j < size; j += 4096) {
-			p[j] = 0;
+			p0[j] = 'a';
 		}
-		printf("alloc_much: 8MB from %p to %p\n", p, p + size);
+		printf("alloc_much: 8MB from %p to %p\n", p0, p0 + size);
 	}
 	{
 		printf("alloc_much: allocating extra 2.5MB memory\n");
 		size = 2 * 1024 * 1024 + 512 * 1024;
-		p = malloc(size);
+		p1 = malloc(size);
 		for (j = 0; j < size; j += 4096) {
-			p[j] = 0;
+			p1[j] = 'b';
 		}
 	}
-	printf("alloc_much: finished\n");
+	printf("alloc_much: finished (%c %c)\n", p0[0], p1[0]);
 	return 0;
 }
